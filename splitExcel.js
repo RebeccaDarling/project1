118890bc92b5947eae15d08e418e541e5144270e
RebeccaDarling <145950129+RebeccaDarling@users.noreply.github.com>
2023-11-10 12:50:56 -0600

    Update splitExcel.js

--- splitExcel.js
+++ splitExcel.js
@@ -7,21 +7,34 @@ function splitExcel() {
         return;
     }
 
+    const folderInput = document.getElementById('outputFolder');
+    if (folderInput.files.length === 0) {
+        alert('Please choose an output folder.');
+        return;
+    }
+
+    const outputFolder = folderInput.files[0];
+
+    const columnName = document.getElementById('columnName').value;
+
+    if (!columnName) {
+        alert('Please specify the column name for separation.');
+        return;
+    }
+
     const reader = new FileReader();
 
     reader.onload = function (e) {
         const data = e.target.result;
         const workbook = XLSX.read(data, { type: 'array' });
 
-        const sheetName = workbook.SheetNames[0]; // Assuming you want to split the first sheet.
+        const sheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[sheetName];
-        
-        // Extract the data as an array of objects
+
         const dataArr = XLSX.utils.sheet_to_json(worksheet);
 
-        // Group data by name from a filtered column (e.g., column A)
         const groupedData = dataArr.reduce((result, row) => {
-            const name = row['Name']; // Adjust 'Name' to the actual header in your Excel file
+            const name = row[columnName];
             if (!result[name]) {
                 result[name] = [];
             }
@@ -29,26 +42,26 @@ function splitExcel() {
             return result;
         }, {});
 
-        // Save each group as a separate Excel file
         for (const name in groupedData) {
             const newWorkbook = XLSX.utils.book_new();
             const newWorksheet = XLSX.utils.json_to_sheet(groupedData[name]);
             XLSX.utils.book_append_sheet(newWorkbook, newWorksheet, 'Sheet1');
             const outputData = XLSX.write(newWorkbook, { bookType: 'xlsx', type: 'blob' });
 
-            // Create a Blob and save the file
             const blob = new Blob([outputData], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-            const url = window.URL.createObjectURL(blob);
-            const a = document.createElement('a');
-            a.href = url;
-            a.download = `${name}_output.xlsx`;
-            a.click();
-            window.URL.revokeObjectURL(url);
+            const outputFilePath = `${outputFolder.webkitRelativePath}/${name}_output.xlsx`;
+            saveAs(blob, outputFilePath);
         }
 
-        alert('Excel file has been split into separate files based on the filtered column.');
+        alert('Excel file has been split into separate files based on the specified column.');
     };
 
     reader.readAsArrayBuffer(file);
 }
 
+function saveAs(blob, fileName) {
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = fileName;
+    link.click();
+}
